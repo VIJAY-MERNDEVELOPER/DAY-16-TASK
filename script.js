1e86dd6909934ba1a7a855fcbd3d2dac65aa7c5a
VIJAY-MERNDEVELOPER <vijay.spm.07@gmail.com>
2023-12-19 16:57:23 +0530

    new change

--- script.js
+++ script.js
@@ -19,9 +19,12 @@ setTimeout(() => {
                 setTimeout(() => {
                   viewPoint.innerHTML = "<h2>2</h2>";
                   setTimeout(() => {
-                    viewPoint.innerHTML = "<h2>1</h2>";
+                    viewPoint.innerHTML = `<h2>1</h2>`;
                     setTimeout(() => {
-                      viewPoint.innerHTML = "<h2>HAPPY INDEPENDENCE DAY </h2>";
+                      viewPoint.innerHTML = `<h2 style="background: radial-gradient(#e42ac2, #2b3ed2, #e6b2ee);
+                      -webkit-background-clip: text;
+                      background-clip: text;
+                      color: transparent;">HAPPY INDEPENDENCE DAY </h2>`;
                     }, 1000);
                   }, 1000);
                 }, 1000);
